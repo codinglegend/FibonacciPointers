8b754baf1c55042c6329f193a8999dceec70068c
Alain K <alain.kahwaji@gmail.com>
2015-05-06 16:43:33 -0700

    use iteration to call nth fibonacci number

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -8,7 +8,34 @@
 
 #include <stdio.h>
 
+//other functions always outside the main function
+// use main function to call other function
+
+int Fibonacci(int nthFibanacciNumber){
+    
+    int previousPreviousVariable = 0; // must be outside for loop, or else thrown out every time within loop
+    int previousVariable = 1;
+    int current = 0;
+    
+    
+    for (int i=1; i<nthFibanacciNumber; i++){
+        
+        current = previousVariable + previousPreviousVariable;
+        
+        previousPreviousVariable = previousVariable;
+        previousVariable = current;
+        
+    }
+    return current;
+}
+
+
 int main()
 {
+    
+    int nth = Fibonacci(10);
+    printf("%d\n", nth);
+    
+
     return 0;
 }
