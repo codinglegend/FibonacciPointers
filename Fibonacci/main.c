c192bd7684d5e0b3890588e5efa6cf5fd1a3ca8d
Alain K <alain.kahwaji@gmail.com>
2015-05-06 21:07:23 -0700

    fix part 1 and add part 2

--- Fibonacci/main.c
+++ Fibonacci/main.c
@@ -11,14 +11,17 @@
 //other functions always outside the main function
 // use main function to call other function
 
-int Fibonacci(int nthFibanacciNumber){
+
+// part 1: iteration
+
+int Fibonacci(int nthFibonacciNumber){ //nthFibonacciNumber is what we input in main function
     
     int previousPreviousVariable = 0; // must be outside for loop, or else thrown out every time within loop
     int previousVariable = 1;
     int current = 0;
     
     
-    for (int i=1; i<nthFibanacciNumber; i++){
+    for (int i=2; i<nthFibonacciNumber; i++){
         
         current = previousVariable + previousPreviousVariable;
         
@@ -29,12 +32,24 @@ int Fibonacci(int nthFibanacciNumber){
     return current;
 }
 
+// part 2: recursion via pass by --> build returned value 144 instead of 55
+
+     int recursiveValueFibonacci(int remainingSteps, int previousNumber, int previousPreviousNumber) {
+     if (remainingSteps == 0) {
+         return previousPreviousNumber + previousNumber;
+     }
+     return recursiveValueFibonacci(remainingSteps - 1, previousPreviousNumber + previousNumber, previousNumber);
+ }
+
+
 
 int main()
-{
-    
+    {
     int nth = Fibonacci(10);
     printf("%d\n", nth);
+        
+    int result = recursiveValueFibonacci(10, 1, 0);
+    printf("%d\n", result);
     
 
     return 0;
